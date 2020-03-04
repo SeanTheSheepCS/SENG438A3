5f0589a28cfeb91f1f48479e3184a4121eb61582
rish-kumaria <rish.kumaria@qhrtech.com>
2020-03-04 14:06:14 -0700

    Added test for contrain method

--- src/org/jfree/data/test/RangeTest.java
+++ src/org/jfree/data/test/RangeTest.java
@@ -40,6 +40,13 @@ public void hashCodeRangeTest() {
 
 }
 
+@Test
+public void constrainRangeTest() {
+	Range newRange = new Range(3,6);
+	double x = newRange.constrain(0);
+	assertTrue(0==x);
+}
+
 @After
 public void tearDown() throws Exception {
 }
